18eae6c22912c61a2865aec915cc2be4120c190a
WEIBIN SONG <sone@silmoon.com>
2026-08-25 12:29:18 +0800

    一个空的LlmClient类，可能需要再次抽象封装大语言模型交互能力。

---
+++ Silmoon.AI/LlmClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Silmoon.AI.Interfaces;
+
+namespace Silmoon.AI
+{
+    public class LlmClient
+    {
+        INativeClient NativeClient { get; set; }
+        public LlmClient(INativeClient nativeClient)
+        {
+            NativeClient = nativeClient;
+        }
+    }
+}
